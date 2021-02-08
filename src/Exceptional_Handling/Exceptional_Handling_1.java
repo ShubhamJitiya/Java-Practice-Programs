c4b29647ddd929cfa337044e95dc66114717d7fb
Shubham Jitiya <jitiya66@gmail.com>
2021-02-09 00:36:55 +0530

    First Exceptional Handling

---
+++ src/Exceptional_Handling/Exceptional_Handling_1.java
@@ -0,0 +1,7 @@
+package Exceptional_Handling;
+
+public class Exceptional_Handling_1 {
+    public static void main(String[] args) {
+        System.out.println("Exceptional Handling in Exceptinal Handling package");
+    }
+}
