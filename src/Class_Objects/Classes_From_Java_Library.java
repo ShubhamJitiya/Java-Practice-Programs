264ae05a2426847a7101b51f1df5e9a82e25f48e
Shubham Jitiya <jitiya66@gmail.com>
2021-03-15 19:26:56 +0530

    classes from java library - date and time

---
+++ src/Class_Objects/Classes_From_Java_Library.java
@@ -0,0 +1,12 @@
+//Using Classes from java library
+
+package Class_Objects;
+import java.util.*;
+
+public class Classes_From_Java_Library {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Today's date is: " + date);
+
+    }
+}
