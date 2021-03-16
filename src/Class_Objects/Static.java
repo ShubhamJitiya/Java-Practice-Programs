ddea56f3b92fde7a30adf7eac9803ad1300a43d0
Shubham Jitiya <jitiya66@gmail.com>
2021-03-16 17:04:54 +0530

    Static

--- src/Class_Objects/Static.java
+++ src/Class_Objects/Static.java
@@ -18,20 +18,29 @@ class Emp
         System.out.println(eid + " : " + salary + " : " + ceo);
     }
 
-    static
+    static //When we load a class - Class is loaded once only - class loads first
     {
         ceo = "Larry";
+        System.out.println("In static");
     }
-    public Emp()
+
+    //If more than one static block exist then only it follows sequence
+
+    public Emp() //When we create an object
     {
         eid = 1;
         salary = 3000;
 //        ceo = "Larry"; //Don't need - Because it creates copy everytime
+        System.out.println("In constructor ...");
     }
 }
 public class Static {
+//    int i;
+//    static int i;
     public static void main(String[] args) {
         Emp navin = new Emp();
+
+//        i = 10; //Only static varibles can be accessed inside static methods
 
 //        navin.eid = 8;
 //        navin.salary = 4000;
@@ -56,3 +65,6 @@ public class Static {
 }
 
 //Same memory is shared for all objects
+//Static Varibles - same for all objects
+//Non-static Variables - NOT SAME
+//Only static varibles can be accessed inside static methods
